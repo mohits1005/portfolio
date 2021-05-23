404cefa832c1e67862eeabcba7048a7ab2b4aba9
mohit sharma <mohit@chalkstreet.com>
2021-05-23 18:44:40 +0530

    added box hidden property

--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -3,18 +3,52 @@ import {
     Link
 } from "react-router-dom";
 import Grid from '@material-ui/core/Grid';
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    gridItem:{}
+})
+
 export default function Nav() {
+    const classes = useStyles();
     return (
         <>
-        <Link to="/" className="link-hover">
-            <div className="user-name">
-            Mohit Sharma
-            </div>
-        </Link><br />
+        <Box
+            component={Grid}
+            className={classes.gridItem}
+            item
+            display={{ xs: "none", sm: "block" }}
+            >
+            <Link to="/" className="link-hover">
+                <div className="user-name">
+                    Mohit Sharma
+                </div>
+            </Link>
+        </Box>
+        <Box
+            component={Grid}
+            className={classes.gridItem}
+            item
+            display={{ xs: "block", sm: "none" }}
+        >
+            <Link to="/" className="link-hover-mobile">
+                <div className="user-name">
+                    Mohit Sharma
+                </div>
+            </Link>
+        </Box>
+        <br />
         <div className="custom-nav">
             <Grid
             container>
-                <Grid item md={2} xs={12}>
+                <Box 
+                    component={Grid}
+                    className={classes.gridItem}
+                    item
+                    md = {2}
+                    display = {{xs: "none", sm: "block"}}
+                >
                     <div className="options">
                         <Link to="/blog" className="link-hover">
                             <div className="link-title">
@@ -22,8 +56,14 @@ export default function Nav() {
                             </div>
                         </Link>
                     </div>
-                </Grid>
-                <Grid item md={4} xs={12}>
+                </Box>
+                <Box
+                    component={Grid}
+                    className={classes.gridItem}
+                    item
+                    md={4}
+                    display={{ xs: "none", sm: "block" }}
+                >
                     <div className="options">
                         <Link to="/projects" className="link-hover">
                             <div className="link-title">
@@ -31,7 +71,37 @@ export default function Nav() {
                             </div>
                         </Link>
                     </div>
-                </Grid>
+                </Box>
+                <Box 
+                    component={Grid}
+                    className={classes.gridItem}
+                    item
+                    xs = {3}
+                    display={{ xs: "block", sm: "none"}}
+                >
+                    <div className="options">
+                        <Link to="/blog" className="link-hover-mobile">
+                            <div className="link-title">
+                                    blog
+                            </div>
+                        </Link>
+                    </div>
+                </Box>
+                <Box
+                    component={Grid}
+                    className={classes.gridItem}
+                    item
+                    xs={3}
+                    display={{ xs: "block", sm: "none" }}
+                >
+                    <div className="options">
+                        <Link to="/projects" className="link-hover-mobile">
+                            <div className="link-title">
+                                    projects
+                            </div>
+                        </Link>
+                    </div>
+                </Box>
             </Grid>
         </div><br />
         </>
